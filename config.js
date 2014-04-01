ac1813782a0ce3a78065da231f133a012718ab9a
Christian G. Warden <cwarden@xerus.org>
2014-04-01 15:27:53 -0700

    Use URI for MongoDB Connection

--- config.js
+++ config.js
@@ -1,15 +1,7 @@
-var mongo_url = process.env.MONGODB_URI;
-var params = /mongodb:\/\/(?:(\w+):(\w+)@)?([\w\.]+)(?::(\d+))?\/(\w+)/.exec(mongo_url);
-
 module.exports = {
-  "mongo-username": params[1],
-  "mongo-password": params[2],
-  "mongo-host": params[3],
-  "mongo-port": parseInt(params[4]),
-  "mongo-database": params[5],
+  "mongo-url": process.env.MONGODB_URI,
   "http-port": process.env.PORT,
   "authenticator": process.env.CUBE_AUTHENTICATOR,
   "consumer-secret": process.env.CONSUMER_SECRET,
   "valid-email": process.env.VALID_EMAIL_DOMAIN
 };
-
